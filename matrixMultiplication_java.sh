607a4f96427b02b8149d086dfb34a600397db74a
Colby Snedeker <Woden501@gmail.com>
2016-03-03 13:04:19 -0500

    Reduce should be done.  Run script updated.  Comments need updated.

--- matrixMultiplication_java.sh
+++ matrixMultiplication_java.sh
@@ -1,13 +1,9 @@
-hadoop fs -mkdir colbySnedekerProject1/input
-hadoop fs -put sample.txt colbySnedekerProject1/input
+hadoop fs -mkdir colbySnedekerProject2/input
+hadoop fs -put sample.txt colbySnedekerProject2/input
 
-mkdir runningMean_class
-javac -classpath /opt/hadoop/hadoop-core-1.2.1.jar:/lib/commons-cli-1.2.jar -d runningMean_class RunningMean.java
-jar -cvf RunningMean.jar -C /home/vcslstudent/colbySnedekerProject1/runningMean_class/ .
-hadoop jar /home/vcslstudent/colbySnedekerProject1/RunningMean.jar snedeker.cc.project1.cluster.RunningMean 3 /user/vcslstudent/colbySnedekerProject1/input/sample.txt /user/vcslstudent/colbySnedekerProject1/RunningMean3_out_java
-hadoop jar /home/vcslstudent/colbySnedekerProject1/RunningMean.jar snedeker.cc.project1.cluster.RunningMean 4 /user/vcslstudent/colbySnedekerProject1/input/sample.txt /user/vcslstudent/colbySnedekerProject1/RunningMean4_out_java
+mkdir matrixMultiplication_class
+javac -classpath /opt/hadoop/hadoop-core-1.2.1.jar:/lib/commons-cli-1.2.jar -d matrixMultiplication_class MatrixMultiplication.java
+jar -cvf MatrixMultiplication.jar -C /home/vcslstudent/colbySnedekerProject2/matrixMultiplication_class/ .
+hadoop jar /home/vcslstudent/colbySnedekerProject2/MatrixMultiplication.jar snedeker.cc.project2.MatrixMultiplication 3 /user/vcslstudent/colbySnedekerProject2/input/sample.txt /user/vcslstudent/colbySnedekerProject2/MatrixMultiplication3_out_java
 
-echo -e "\n\nWindow Size of 3:\n"
-hadoop fs -cat /user/vcslstudent/colbySnedekerProject1/RunningMean3_out_java/part-r-00000
-echo -e "\n\nWindow Size of 4:\n"
-hadoop fs -cat /user/vcslstudent/colbySnedekerProject1/RunningMean4_out_java/part-r-00000
+hadoop fs -cat /user/vcslstudent/colbySnedekerProject2/MatrixMultiplication_out_java/part-r-00000
--- src/snedeker/cc/project2/MatrixMultiplication.java
+++ src/snedeker/cc/project2/MatrixMultiplication.java
@@ -1,17 +1,13 @@
 package snedeker.cc.project2;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.DecimalFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -24,8 +20,6 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class MatrixMultiplication {
 	
-	final static private int WINDOW_SIZE = 3;
-
 	/**
 	 * This is the Mapper component.  It will take the input data and separate it into
 	 * individual entries which will later be combined and averaged.
@@ -73,26 +67,6 @@ public class MatrixMultiplication {
 					context.write(resultPosition, multiplicationValue);
 				}
 			}
-			
-//			// Create a simple date formatter for reading the date
-//			DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-//			format.setLenient(false);
-//			Date date = null;
-//			try {
-//				date = format.parse(values[1]);
-//			} catch (ParseException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			// Convert the Java Date into a long value
-//			long longDate = date.getTime();
-//			
-//			// Set the values in the Time_Series
-//			series.set(longDate, Double.parseDouble(values[2]));
-//			// Set the word to be the company code
-//			word.set(values[0]);
-//			// Write the values back out to the mapper context
-//			context.write(word, series);
 		}
 	}
 	
@@ -103,7 +77,7 @@ public class MatrixMultiplication {
 	 * @author Colby Snedeker
 	 *
 	 */
-	public static class Reduce extends Reducer<Text, Text, Text, Text> {
+	public static class Reduce extends Reducer<Text, Text, Text, DoubleWritable> {
 
 		/**
 		 * This is the reduce function.  It iterates through all of the Time_Series values to 
@@ -111,65 +85,42 @@ public class MatrixMultiplication {
 		 * value pair to the Reducer context that consists of the company code as the key, 
 		 * and a string providing the means in a formatted way as the value.
 		 */
-		public void reduce(Text key, Text values, Context context) throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<Text> multiplicationValues, Context context) throws IOException, InterruptedException {
 			
-//			Configuration conf = context.getConfiguration();
-//			int windowSize = Integer.parseInt(conf.get("window.size"));
-//			
-//			int seriesCount = 0;
-//			LinkedList<Double> prices = new LinkedList<>();
-//			String companyCode = key.toString();
-//			
-//			// Sort the Time_Series by date
-//			TreeMap<Long, Double> sortedSeries = new TreeMap<>();
-//			for (Time_Series series : values) {
-//				sortedSeries.put(series.getTimestamp(), series.getValue());
-//			}
-//			
-//			// Iterate through the series for the company, and compute both the 3 and 4 window means
-//			for (java.util.Map.Entry<Long, Double> entry : sortedSeries.entrySet()) {
-//				Text means = new Text();
-//				double total = 0;
-//				
-//				seriesCount++;
-//				
-//				if (prices.size() >= windowSize)
-//					prices.remove();
-//				
-//				prices.add(entry.getValue());
-//				
-//				double mean = 0;
-//				
-//				if (seriesCount < windowSize) {
-//					for (int i = 0; i < seriesCount; i++) {
-//						total += prices.get(i);
-//					}
-//					
-//					mean = total / (double) seriesCount;
-//				}
-//				else {
-//					for (int i = 0; i < windowSize; i++) {
-//						total += prices.get(i);
-//					}
-//					
-//					mean = total / (double) windowSize;
-//				}
-//				
-//				DecimalFormat df = new DecimalFormat("#.00");
-//
-//				// Write the output string to the means variable
-//				means.set(df.format(mean));
-//				
-//				SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
-//				String date = dateFormatter.format(new Date(entry.getKey()));
-//				
-//				// Properly set the key
-//				key.set(companyCode + "," + date);
-//					
-//				// Write the company code and means to the reducer context
-//				context.write(key, means);
-//			}
+			DoubleWritable output = new DoubleWritable();
 			
+			TreeMap<Integer, ArrayList<Double>> jOrderedValues = new TreeMap<>();
+			double sum = 0;
+			
+			// Read the multiplicationValues and insert the j position and value into a key value pair {j position, value}.
+			// In this case we don't care what matrix the value is from as we are just going to be multiplying all values
+			// with the same j key together later.
+			for (Text text : multiplicationValues) {
+				String multiplicationValue = text.toString();
+				String[] values = multiplicationValue.split(",");
+				
+//				jOrderedValues.put(Integer.parseInt(values[1]), Double.parseDouble(values[2]));
+				
+				ArrayList<Double> positionValues = jOrderedValues.get(Integer.parseInt(values[1]));
+				
+				if (positionValues != null) {
+					positionValues.add(Double.parseDouble(values[2]));
+				}
+				else {
+					positionValues = new ArrayList<>();
+					positionValues.add(Double.parseDouble(values[2]));
+					jOrderedValues.put(Integer.parseInt(values[1]), positionValues);
+				}
+			}
+			
+			for (Entry<Integer, ArrayList<Double>> entry : jOrderedValues.entrySet()) {
+				ArrayList<Double> values = entry.getValue();
+				sum += values.get(0) * values.get(1);
+			}
+			
+			output.set(sum);
+			
+			context.write(key, output);
 		}
 	}
 	
@@ -184,11 +135,11 @@ public class MatrixMultiplication {
 		Configuration conf = new Configuration();
 		conf.set("window.size", args[0]);
 		conf.set("mapred.textoutputformat.separator", ",");
-		Job job = new Job(conf, "runningMean");
+		Job job = new Job(conf, "matrixMultiplication");
 		job.setJarByClass(snedeker.cc.project2.MatrixMultiplication.class);
 		
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputValueClass(DoubleWritable.class);
 		//Set key, output classes for the job (same as output classes for Reducer)
 		job.setMapperClass(Map.class);
 		job.setReducerClass(Reduce.class);
